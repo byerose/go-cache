820bf0b9c1e13c4d6e8f3a1a045cde9a87a3ee67
byerose <81293778+byerose@users.noreply.github.com>
2022-07-18 23:48:19 +0800

    go compile

--- main.go
+++ main.go
@@ -1,19 +1,19 @@
 package main
 
-import (
-	"log"
-	"net/http"
-)
-
-type server int
-
-func (h *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.URL.Path)
-	w.Write([]byte("Hello World!"))
-}
-
 func main() {
-	var s server
-	//第一个参数是服务启动的地址，第二个参数是 Handler，任何实现了 ServeHTTP 方法的对象都可以作为 HTTP 的 Handler。
-	http.ListenAndServe("localhost:9999", &s)
+	println("hello,world.")
 }
+
+// day3 - http server
+//type server int
+//
+//func (h *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+//	log.Println(r.URL.Path)
+//	w.Write([]byte("Hello World!"))
+//}
+//
+//func main() {
+//	var s server
+//	//第一个参数是服务启动的地址，第二个参数是 Handler，任何实现了 ServeHTTP 方法的对象都可以作为 HTTP 的 Handler。
+//	http.ListenAndServe("localhost:9999", &s)
+//}
